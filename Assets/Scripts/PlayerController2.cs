8e9fd4974106c94ffa972e70134a86b262f2dd70
cahyonline <cahyonurrohman46@gmail.com>
2024-09-20 15:29:19 +0700

    y

--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController2 : MonoBehaviour
 {
     private Rigidbody rb;
     public float speed;
@@ -19,29 +19,28 @@ public class PlayerController : MonoBehaviour
     }
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-        rb.AddForce (movement*speed);
+        float moveHorizontal = Input.GetAxis("Horizontal2");
+        float moveVertical = Input.GetAxis("Vertical2");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if(isGrounded == true)
+            if (isGrounded == true)
             {
                 Debug.Log("space");
                 rb.AddForce(Vector3.up * jumpForce * Time.deltaTime, ForceMode.Impulse);
             }
-            
+
         }
     }
-
     public void OnCollisionEnter(Collision collision)
     {
-        
+
         if (collision.gameObject.CompareTag("Item"))
         {
             coin++;
@@ -56,9 +55,11 @@ public class PlayerController : MonoBehaviour
 
     public void OnCollisionExit(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Ground")) {
-        
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+
             isGrounded = false;
         }
     }
+
 }
--- Assets/Scripts/menu.cs
+++ Assets/Scripts/menu.cs
@@ -8,11 +8,13 @@ public class menu : MonoBehaviour
 {
     public GameObject MenuPanel;
     public GameObject InfoPanel;
+    public GameObject CreditPanel;
 
     void Start()
     {
         MenuPanel.SetActive(true);
         InfoPanel.SetActive(false);
+        CreditPanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -29,13 +31,22 @@ public class menu : MonoBehaviour
     {
         MenuPanel.SetActive(false );
         InfoPanel.SetActive(true);
+        CreditPanel.SetActive(false);
     }
 
     public void BackButton()
     {
         MenuPanel.SetActive(true );
         InfoPanel.SetActive(false);
-        Debug.Log("back");
+        CreditPanel.SetActive(false);
 
+    }
+
+    public void CreditButton()
+    {
+        MenuPanel.SetActive(false );
+        InfoPanel.SetActive(false);
+        CreditPanel.SetActive(true);
+        Debug.Log("credit");
     }
 }
